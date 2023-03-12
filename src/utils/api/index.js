ebd481a08839966244378dc28f6b8f5e9f42ec04
Muhammad Abdusamad <muhammadabdusamad@yahoo.com>
2023-03-12 02:45:16 +0000

    updates

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -2,7 +2,7 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = 'https://flashcard-thinkful-gmalx4o26-hyder.vercel.app/'
+const API_BASE_URL = 'https://flashcard-thinkful-gmalx4o26-hyder.vercel.app'
 // process.env.API_BASE_URL || "http://localhost:5000";
 
 /**
